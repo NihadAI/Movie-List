dc0504aedf457bbc0d40c29440ee4a70aaaefda4
Nihad <nihadalizadaxx@gmail.com>
2024-12-04 12:40:31 +0100

    commit

--- src/components/header/Dropdownuser.tsx
+++ src/components/header/Dropdownuser.tsx
@@ -65,7 +65,7 @@ const DropdownUser = ({ user }: { user: any }) => {
           dropdownOpen === true ? 'block' : 'hidden'
         }`}
       >
-        <SignOutButton>
+        <SignOutButton redirectUrl='/sign-in'>
             <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
               <LogOut/>
               Log Out
